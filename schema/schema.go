36e43d21e5469c91fd380f316f4c4155a50e0bea
chaewonkong <chaewonkong@gmail.com>
2025-07-08 22:21:12 +0900

    feat: add simple schema

---
+++ schema/schema.go
@@ -0,0 +1,23 @@
+package schema
+
+// Ticket represents a matchmaking ticket.
+type Ticket struct {
+	ID        string   `json:"id"`
+	PlayerIDs []string `json:"player_ids"`
+}
+
+// Player represents a player.
+type Player struct {
+	ID string `json:"id"`
+}
+
+// Match represents a match
+type Match struct {
+	ID      string   `json:"id"`
+	Tickets []Ticket `json:"tickets"`
+}
+
+// MatchResult represents the result of a match.
+type MatchResult struct {
+	MatchID string `json:"match_id"`
+}
